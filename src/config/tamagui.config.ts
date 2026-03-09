bdd4a84fe79762d3886123e4dda249ba8e1d980b
marketingtool <infinity@antiviruspoint.org>
2026-03-08 20:19:18 -0700

    fix(build): final isolation of Tamagui config and strict easignore

---
+++ src/config/tamagui.config.ts
@@ -0,0 +1,19 @@
+import { createTamagui } from 'tamagui'
+import { config as configV3 } from '@tamagui/config/v3'
+
+export const config = createTamagui({
+  ...configV3,
+  themes: {
+    ...configV3.themes,
+    light: configV3.themes?.light || {
+      background: '#fff',
+      color: '#000',
+    },
+    dark: configV3.themes?.dark || {
+      background: '#060b28',
+      color: '#fff',
+    },
+  },
+})
+
+export default config
