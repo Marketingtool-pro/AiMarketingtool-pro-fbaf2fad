8a95909fcf55f6f23ec42d02f6ac509647c47b1d
marketingtoolpro <304695078+help772@users.noreply.github.com>
2026-09-10 04:15:07 +0530

    chore(repo): delete one-off codemod fix-toolicons.js
    Signed-off-by: marketingtoolpro <304695078+help772@users.noreply.github.com>

--- fix-toolicons.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/constants/toolIcons.ts', 'utf8');
-
-code = code.replace(
-  'import { Platform } from "react-native";',
-  '// Unused platform import removed'
-);
-
-fs.writeFileSync('src/constants/toolIcons.ts', code);
